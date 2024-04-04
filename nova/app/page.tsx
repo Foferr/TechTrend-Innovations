b4d7b7c4b8c2d6d64a8d7e4ddf2bcf2c83232561
valdezae <A00829909@itesm.mx>
2024-04-04 00:52:40 -0600

    Edited to match Neoris' brand identity, added social log in buttons

--- nova/app/page.tsx
+++ nova/app/page.tsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 export default function Home() {
   return (
       <div className="relative flex flex-col p-32 items-end justify-center min-h-screen overflow-hidden">
-        <div className="w-full p-6 bg-white rounded-md shadow-md lg:max-w-xl">
-          <h1 className="text-3xl font-bold text-center text-gray-700">Login</h1>
+        <div className="w-full p-6 bg-neoris-grey-100 rounded-md shadow-md lg:max-w-xl">
+          <h1 className="text-3xl font-bold text-center ttext-neoris-white-100">Inicio de sesión</h1>
           <form className="mt-6">
             <div className="mb-4">
               <label
                   htmlFor="email"
-                  className="block text-sm font-semibold text-gray-800"
+                  className="block text-sm font-semibold text-neoris-white-100"
               >
-                User
+                Usuario
               </label>
               <input
                   type="email"
@@ -20,9 +20,9 @@ export default function Home() {
             <div className="mb-2">
               <label
                   htmlFor="password"
-                  className="block text-sm font-semibold text-gray-800"
+                  className="block text-sm font-semibold text-neoris-white-100"
               >
-                Password
+                Contraseña
               </label>
               <input
                   type="password"
@@ -30,21 +30,52 @@ export default function Home() {
               />
             </div>
             <div className="mt-2">
-              <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-                Login
+              <button className="w-full px-4 py-2 tracking-wide bg-neoris-white-100 text-neoris-grey-100 rounded-md hover:bg-neoris-white-50 active:bg-neoris-grey-50 active:text-neoris-white-100">
+                Iniciar sesión
               </button>
             </div>
           </form>
 
-          <p className="mt-4 text-sm text-center text-gray-700">
-            Don&apos;t have an account?{" "}
+          <p className="mt-4 text-sm text-center text-neoris-white-100">
+            ¿No tienes cuenta?{" "}
             <Link
                 href="/signup"
                 className="font-medium text-blue-600 hover:underline"
             >
-              Sign up
+              Registrate
             </Link>
           </p>
+
+          <div className="relative flex pt-10 pb-5 items-center">
+            <div className="flex-grow border-t border-gray-400"></div>
+            <span className="flex-shrink mx-4 text-neoris-white-100 text-opacity-50">O continua con</span>
+            <div className="flex-grow border-t border-gray-400"></div>
+          </div>
+
+          <div className="mt-1 grid grid-cols-3 gap-3">
+            <div>
+              <a href="#"
+                 className="w-full flex items-center justify-center px-8 py-3 rounded-md shadow-sm text-sm font-medium bg-neoris-white-100 hover:bg-neoris-white-50 active:bg-neoris-grey-50 active:text-neoris-white-100">
+                <img className="h-5 w-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/512px-2021_Facebook_icon.svg.png?20220821121039"
+                     alt=""/>
+              </a>
+            </div>
+            <div>
+              <a href="#"
+                 className="w-full flex items-center justify-center px-8 py-3 rounded-md shadow-sm text-sm font-medium bg-neoris-white-100 hover:bg-neoris-white-50 active:bg-neoris-grey-50 active:text-neoris-white-100">
+                <img className="h-5 w-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/120px-Google_%22G%22_logo.svg.png?20230822192911"
+                     alt=""/>
+              </a>
+            </div>
+            <div>
+              <a href="#"
+                 className="w-full flex items-center justify-center px-8 py-3 rounded-md shadow-sm text-sm font-medium bg-neoris-white-100 hover:bg-neoris-white-50 active:bg-neoris-grey-50 active:text-neoris-white-100">
+                <img className="h-5 w-5" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/X_logo_2023.svg/300px-X_logo_2023.svg.png?20230819000805"
+                     alt=""/>
+              </a>
+            </div>
+          </div>
+
         </div>
       </div>
   );
--- nova/tailwind.config.ts
+++ nova/tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'neoris-grey': {
+          50: 'rgb(74,81,87)',
+          100: 'rgb(29,37,45)'
+        },
+
+        'neoris-white': {
+          50: 'rgb(240,240,238)',
+          100: 'rgb(217,217,214)'
+        },
       },
     },
   },
