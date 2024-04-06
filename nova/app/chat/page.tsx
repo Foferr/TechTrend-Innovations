7ac6955a812fd0ce5ff49bfe7ccad9d0cbe65741
Mariano Barberi Garza <88672243+MarianoBarberi@users.noreply.github.com>
2024-04-05 18:55:56 -0600

    first changes

--- nova/app/chat/page.tsx
+++ nova/app/chat/page.tsx
@@ -1,6 +1,21 @@
 import Link from "next/link";
+import Image from "next/image";
+import "./styles.css";
+import "../globals.css";
+
 export default function Chat() {
     return (
-        <div></div>
+        <body>
+            <div className="upperDiv">
+                <Image
+                    src="/Brand/Icon/Icon dark.png"
+                    alt=""
+                    width={100}
+                    height={100}
+                />           
+                <h1>Noticias</h1>
+                <img src="/images/threelines.jpg" alt="" className="w-10"/>
+            </div>
+        </body>
     );
 }
