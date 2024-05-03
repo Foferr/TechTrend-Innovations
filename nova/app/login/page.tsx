ac3d5da254c8d1715283ef49256ae6b6a782a629
valdezae <A00829909@itesm.mx>
2024-05-02 22:46:34 -0600

    Rebranded landing

--- nova/app/login/page.tsx
+++ nova/app/login/page.tsx
@@ -4,12 +4,13 @@ export default function Home() {
     return (
 
         <div
-            className="relative flex flex-row space-x-52 bg-neoris-grey-100 items-center justify-start min-h-screen overflow-hidden">
-
-            <div className="relative flex flex-col pl-20 space-y-8">
+            className="relative flex flex-row bg-neoris-grey-100 items-center justify-start min-h-screen overflow-hidden">
+            <video autoPlay loop muted className="absolute -z-0 w-auto min-w-full min-h-full max-w-none" src="/images/121799-724719792.mp4"></video>
+            <div className="relative flex flex-col pl-24 space-y-8">
                 <div className="animatedText -mb-16">Nova</div>
-                <h2 className="text-neoris-white-100 font-semibold">Conoce los productos que Neoris tiene que ofrecer</h2>
+                <h2 className="text-neoris-white-100 text-2xl w-full">Conoce los productos que Neoris tiene que ofrecer</h2>
             </div>
+
 
         </div>
     );
