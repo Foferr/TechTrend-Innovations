100ec82b89648e552cab6f341267535fa0513d40
Emiliano_Salazar <emisalazar568@gmail.com>
2024-06-04 18:35:29 -0600

    diseño-listo

--- nova/app/admin/users/EditUser.tsx
+++ nova/app/admin/users/EditUser.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from 'axios';
 
+
+
 type User = {
   id: string;
   username: string;
