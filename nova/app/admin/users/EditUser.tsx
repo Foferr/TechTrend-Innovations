efd63212c0d9da1d95ed895ff4b910b1cfc7d48f
Emiliano_Salazar <emisalazar568@gmail.com>
2024-06-04 18:23:22 -0600

    USUARIOS FUNCIONALES

--- nova/app/admin/users/EditUser.tsx
+++ nova/app/admin/users/EditUser.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from 'axios';
-import "./styles3.css"; 
 
 type User = {
   id: string;
