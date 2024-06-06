58b502f1761eaa6c5ef89aa537567c7c5f4c5c32
valdezae <A00829909@itesm.mx>
2024-06-05 22:04:01 -0600

    Added interactive dashboard

---
+++ nova/app/embedtest/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import withAuth from '../components/HOC/withAuth';
+
+
+const embed: React.FC = () => {
+    return (
+
+        <div>
+
+            <iframe className="w-full h-screen"
+                    src="https://lookerstudio.google.com/embed/reporting/c46b5ab3-d017-47eb-83da-c32c64a57bc7/page/Rov1D"
+                    allowFullScreen
+                    sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+        </div>
+    );
+}
+
+export default withAuth(embed);
